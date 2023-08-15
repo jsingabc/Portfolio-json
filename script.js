10cb2ec75e866b6e95a75bdea1ab963fd1e42e08
Jermain Singleton <jsingabc@gmail.com>
2023-08-15 18:58:50 -0400

    This is the best version where data displays
    this is the best version of my code where i can get user input and text output

--- script.js
+++ script.js
@@ -1,62 +1,64 @@
 //testing javascript file sync to html
 console.log("Hello World")
 
+//button function to clear text off screen
+var clrBtn = document.getElementById("btnClr")
+clrBtn.addEventListener("click", function(){
+  let out = "";
+  document.getElementById("demo").innerHTML = out;
+})
 
+// function to make a user query wity fetch
 var myBtn = document.getElementById("myBtn")
 myBtn.addEventListener("click", function(){
   let userAnswer = document.getElementById("answer").value
+  //test output
   console.log(userAnswer)
   const site = 'https://pokeapi.co/api/v2/'
   let url = site + userAnswer
+  //test output
   console.log(url)
 
   fetch(url)
     // returns json as a object so it works
     .then(function(response) { return response.json(); })
-    // data is sent to a function call
+    // call function
     .then(data => showText(data))
+    // call function
+    //.then(data => showTable(data))
     // catches my errors
     .catch(error => console.log(error.message))
 
+    //function takes fetch data and displays in htnl div
     function showText(data) {
-      data = JSON.stringify(data)
+      let out = "";
+      out = JSON.stringify(data)
       console.log(data)
+
+      out += data;
+      document.getElementById("demo").innerHTML = out;
     }
-
-})
-
-
-//variable that holds the fetch file
-
-
-
-
-let out = "";
-
-//function showText() {
-  //data = JSON.stringify(data)
-  //out += data
-  //document.getElementById("test").innerHTML = out;
-//}
-
-
-
+  })
+/*
+     function showTable(data){ 
+      let tableOut = "";
+      let placeholder = document.querySelector("#data-output");
+      tableOut = JSON.stringify(data)
+      for(let poke in data) {
+        tableOut += `
+      <tr>
+        <td>${poke.name}</td>
+        <td>${poke.url}</td>
+        <td>${poke.previous}</td>
+        <td>${poke.results}</td>
+      </tr>
+      `;
+      }
+      placeholder.innerHTML = tableOut;
+    }
+    */
 
     
-    //function to put data to html
-    //function showText(data){
-     //let placeholder = document.querySelector("#data-output");
-     //let out = "";
-     //out = JSON.stringify(data)
-     //for(let product of data) {
-      //out += `
-      //<tr>
-        //<td>${product}</td>
-      //</tr>
-      //`;
-     //}
-      //placeholder.innerHTML = out;
-    //}
 
 
     //function showText(data){
