bf332912d3dbb316f8624f40916a25a8ca96f4ae
Jermain Singleton <jsingabc@gmail.com>
2023-08-18 14:00:25 -0400

    Finished JSON project for practice
    I have added a image, and CSS to my project. Right not you query a pokemon api website and the return is displayed in string objects opn the screen. If i fix, I want formatted html elements.

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 //testing javascript file sync to html
 console.log("Hello World")
 
+textBody = document.getElementById("textBody")
+
+//change submit button color
+myButton = document.getElementById("myBtn")
+myButton.style.backgroundColor = "orange"
+
+//change clear button color
+myClr = document.getElementById("btnClr")
+myClr.style.backgroundColor = "red"
+
+//add image to div on textbody
+myImage = document.getElementsByClassName("myImg")
+myImage = new Image(500, 200)
+myImage.src = "poke1.png"
+textBody.appendChild(myImage);
+
+
 //button function to clear text off screen
 var clrBtn = document.getElementById("btnClr")
 clrBtn.addEventListener("click", function(){
@@ -29,7 +46,7 @@ myBtn.addEventListener("click", function(){
     // catches my errors
     .catch(error => console.log(error.message))
 
-    //function takes fetch data and displays in htnl div
+    //function takes fetch data and displays in html div
     function showText(data) {
       let out = "";
       out = JSON.stringify(data)
@@ -38,42 +55,7 @@ myBtn.addEventListener("click", function(){
       out += data;
       document.getElementById("demo").innerHTML = out;
     }
-  })
-/*
-     function showTable(data){ 
-      let tableOut = "";
-      let placeholder = document.querySelector("#data-output");
-      tableOut = JSON.stringify(data)
-      for(let poke in data) {
-        tableOut += `
-      <tr>
-        <td>${poke.name}</td>
-        <td>${poke.url}</td>
-        <td>${poke.previous}</td>
-        <td>${poke.results}</td>
-      </tr>
-      `;
-      }
-      placeholder.innerHTML = tableOut;
-    }
-    */
+    // add data from fetch to html
+    let list = document.getElementById("list");
 
-    
-
-
-    //function showText(data){
-      //let placeholder = document.querySelector("#data-output");
-      //let out = "";
-      //out = JSON.stringify(data)
-      //for(let product of data) {
-       //out += `
-       //<tr>
-         //<td>${product.name}</td>
-         //<td>${product.year}</td>
-         //<td>${product.color}</td>
-         //<td>${product.pantone_value}</td>
-       //</tr>
-       //`;
-      //}
-       //placeholder.innerHTML = out;
-     //}
+  }) 
